85e664405dd78830479fe6b621d5e04a743d25c4
Hendrik Abdalla Hermann <48130931+HendrikAH12@users.noreply.github.com>
2022-02-20 17:31:51 -0300

    Last Release

--- scraper/main.py
+++ scraper/main.py
@@ -1,33 +1,52 @@
-import bs4
+import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
-import json
+import bs4, time
 
-urls = [
+def create_driver():
+    ua = UserAgent()
+    opts = Options()
+    opts.add_argument("user-agent="+ua.random)
+    opts.add_argument("--headless")
+    return webdriver.Chrome(options=opts)
+
+def get_gpu(url, webdriver=None):
+    gpus = []
+
+    def append_gpu(driver):
+        driver.get(url)
+        time.sleep(5)
+        html = driver.page_source
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        item = soup.find("a", {"class": "ctn-produto"})
+
+        gpus.append({
+            "name": item.find("div", {"class": "pnome"}).text[15:70],
+            "price": item.find("span", {"class": "pvalor"}).findChildren("strong", recursive=True)[0].text[4:]
+        })
+
+    if webdriver:
+        append_gpu(webdriver)
+    else:
+        webdriver = create_driver()
+        append_gpu(webdriver)
+        webdriver.quit()
+
+    return gpus
+
+links = [
     "https://m.kabum.com.br/busca?string=rtx+3050",
     "https://m.kabum.com.br/busca?string=rx+6600",
     "https://m.kabum.com.br/busca?string=rtx+2060",
 ]
 
-ua = UserAgent()
-opts = Options()
-opts.add_argument("user-agent="+ua.random)
-driver = webdriver.Chrome(options=opts)
-gpus = []
+def scraper():
+    time_start = time.time()
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_gpu, link) for link in links]
+        return_value = [f.result()[0] for f in futures]
 
-for url in urls:
-    driver.get(url)
+        return return_value, "{:.2f}".format(time.time() - time_start)
 
-    html = driver.page_source
-    soup = bs4.BeautifulSoup(html, "html.parser")
-    items = soup.find_all("a", {"class": "ctn-produto"})
-
-    for product in items:
-        gpus.append({
-            "name": product.find("div", {"class": "pnome"}).text[15:75],
-            "price": product.find("span", {"class": "pvalor"}).findChildren("strong", recursive=True)[0].text
-        })
-        break
-
-print(json.dumps(gpus, indent=4, sort_keys=True))
+print(scraper())
