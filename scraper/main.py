144af295552390247b8d0e2143d8a48055c188e5
Hendrik Abdalla Hermann <48130931+HendrikAH12@users.noreply.github.com>
2022-03-13 17:17:50 -0300

    Added field to visualize the price change of the GPU

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,7 @@
+from ast import Break
 import concurrent.futures
+from gettext import NullTranslations
+from sys import breakpointhook
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
@@ -24,7 +27,8 @@ def get_gpu(url, webdriver=None):
         for product in items:
             gpus.append({
                 "name": product.find("div", {"class": "pnome"}).text[15:70],
-                "price": product.find("span", {"class": "pvalor"}).findChildren("strong", recursive=True)[0].text[4:]
+                "price": int(product.find("span", {"class": "pvalor"}).findChildren("strong", recursive=True)[0].text[4:9].replace(".", "")),
+                "change": 0
             })
 
     if webdriver:
@@ -37,18 +41,28 @@ def get_gpu(url, webdriver=None):
     return gpus
 
 links = [
-    "https://m.kabum.com.br/busca?string=rtx+3050",
     "https://m.kabum.com.br/busca?string=rx+6600",
-    "https://m.kabum.com.br/busca?string=rtx+2060",
+    "https://m.kabum.com.br/busca?string=rtx+3050",
+    "https://m.kabum.com.br/busca?string=rtx+2060"
 ]
 
 def scraper():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [executor.submit(get_gpu, link) for link in links]
         returned_values = [f.result()[0] for f in futures]
-        
+
+    try:
+        f = open("data.txt")
+        data = json.load(f)
+        c = 0
+        for i in data["gpus"]:
+            returned_values[c]["change"] = returned_values[c]["change"] - (i["price"] - int(returned_values[c]["price"]))
+            c = c + 1
+    except:
+        pass
+
     with open("data.txt", "w") as outfile:
-        outfile.write(json.dumps(returned_values, indent=4))
+        outfile.write(json.dumps({"gpus": returned_values}, indent=4))
 
 if __name__ == "__main__":
     scraper()
